4587fa9002548e8c881043b390ee66b61a0bcb21
GeraldRC <gerald.reyes@outlook.cl>
2020-06-24 11:54:05 -0400

    first commit

---
+++ src/com/controlador/ServletLogin.java
@@ -0,0 +1,65 @@
+package com.controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet("/login")
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletLogin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	
+		response.setContentType("text/html");
+		
+		
+	    PrintWriter out = response.getWriter();
+	    request.getRequestDispatcher("login.jsp").include(request,response);
+		String usu = request.getParameter("usuario");
+		String con = request.getParameter("contrasena");
+		
+		
+		if(usu.trim().length()== 0 || con.trim().length()== 0){
+			out.print("<script>\r\n" + 
+					"        alert(\"Ingrese Datos\");\r\n" + 
+					"    </script>");
+		}else if(usu.equalsIgnoreCase("gerald") && con.equalsIgnoreCase("123")) {
+			HttpSession session=request.getSession();  
+	        session.setAttribute("usuario",usu);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+		}else {
+			out.print("<script>\r\n" + 
+					"        alert(\"Error de Datos\");\r\n" + 
+					"    </script>");  
+		}
+		
+	}
+}
