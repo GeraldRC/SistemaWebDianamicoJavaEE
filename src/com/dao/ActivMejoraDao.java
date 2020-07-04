6e3c11dd65c1204bb492ee49c469f67417519663
Edgardo_Mora <edgardomora@hotmail.com>
2020-07-04 18:40:48 -0400

    Primer cambio Actividad Mejora

--- src/com/dao/ActivMejoraDao.java
+++ src/com/dao/ActivMejoraDao.java
@@ -1,8 +1,12 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.model.ActivMejora;
+import com.conexion.Conexion;
 import com.interfaces.IActivMejoraDao;
 
 public class ActivMejoraDao implements IActivMejoraDao {
@@ -10,6 +14,25 @@ public class ActivMejoraDao implements IActivMejoraDao {
 	@Override
 	public boolean registrarAcme(ActivMejora acme) {
 		// TODO Auto-generated method stub
+			boolean registrar = false;
+		
+			Statement stm = null;
+			Connection con = null;
+		
+			String sql = "INSERT INTO CHEKLIST VALUES (null,'"+acme.getAcme()+"')";
+		
+
+			try {
+				con = Conexion.getConexion();
+				stm = con.createStatement();
+				stm.execute(sql);
+				registrar = true;
+				stm.close();
+			
+			}catch(SQLException e) {
+			System.out.println("Error: ");
+			e.printStackTrace();
+		}
 		return false;
 	}
 
--- src/com/model/ActivMejora.java
+++ src/com/model/ActivMejora.java
@@ -8,7 +8,31 @@ public class ActivMejora {
 	public ActivMejora() {
 		
 	}
-	
-	
+
+	public ActivMejora(String acme) {
+		this.acme = acme;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAcme() {
+		return acme;
+	}
+
+	public void setAcme(String acme) {
+		this.acme = acme;
+	}
+
+	@Override
+	public String toString() {
+		return "ActivMejora [id=" + id + ", acme=" + acme + "]";
+	}
+		
 
 }
