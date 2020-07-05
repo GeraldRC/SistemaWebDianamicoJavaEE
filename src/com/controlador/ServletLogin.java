984b5ce2b3fec9895d638cdf4a082e6744d2f549
Gerald Matias Reyes Castillo <60944300+GeraldRC@users.noreply.github.com>
2020-07-04 23:02:54 -0400

    commit problema

--- src/com/controlador/ServletLogin.java
+++ src/com/controlador/ServletLogin.java
@@ -37,7 +37,8 @@ public class ServletLogin extends HttpServlet {
         String usu = request.getParameter("usuario");
         String con = request.getParameter("contrasena");
         String rol ="";
-
+        System.out.println(usu);
+        System.out.println(con);
         UsuarioDao dao = new UsuarioDao();
         
         Usuario u = dao.comprobarUsuario(usu,con);
@@ -69,7 +70,7 @@ public class ServletLogin extends HttpServlet {
 	            request.getRequestDispatcher("PanelAdmin.jsp").forward(request, response);
 			}
 		}else {
-			 out.print("<p>Error de datos</p>");
+			 out.println("<p>Error de datos</p>");
 
 		}
         
